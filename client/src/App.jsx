f893ecfbbafadd3021c225c3fa93feda8ce734aa
rushichavan21 <rushichavan2327@gmail.com>
2025-04-10 17:29:45 +0530

    minors

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import SnapPage from './pages/Snap/SnapPage'
 import Home from './pages/Home/Home'
 import Discuss from './pages/Discuss/Interview'
 import Experience from './Components/Experience/Experience'
+import VjtiCustom from './pages/vjticustom/vjticustom'
 
 const App = () => {
   const {user,loading}=useAuthContext();
@@ -19,6 +20,7 @@ const App = () => {
         <Route path='/login' element={!user? <Login/>:<Navigate to="/"/>}/>
         <Route path='/signup' element={!user?<SignupPage/>:<Navigate to='/'/>}/>
         <Route path='/' element={user?<Home/>:<Navigate to="/login"/>}/>
+        <Route path='/vjti' element={user?<VjtiCustom/>:<Navigate to="/login"/>}/>
         <Route path='/generate_snap' element={user?<SnapPage/>:<Navigate to="/login"/>}/>
         <Route path='/discuss' element={user?<Discuss/>:<Navigate to="/login"/>}/>
         <Route path='/experience/:id' element={user?<Experience/>:<Navigate to="/login"/>}/>
---
+++ client/src/Components/Halo/Halo.jsx
@@ -0,0 +1,8 @@
+import "./Halo.css"
+const Halo = () => {
+  return (
+    <div>Halo</div>
+  )
+}
+
+export default Halo
