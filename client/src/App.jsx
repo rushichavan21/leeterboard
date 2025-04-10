ecfae70c1013787913bcb05af039a40908f9cfba
rushichavan21 <rushichavan2327@gmail.com>
2025-04-10 17:34:27 +0530

    deploy error fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import SnapPage from './pages/Snap/SnapPage'
 import Home from './pages/Home/Home'
 import Discuss from './pages/Discuss/Interview'
 import Experience from './Components/Experience/Experience'
-import VjtiCustom from './pages/vjticustom/vjticustom'
+import Vjticustom from './pages/vjticustom/Vjticustom'
 
 const App = () => {
   const {user,loading}=useAuthContext();
@@ -20,7 +20,7 @@ const App = () => {
         <Route path='/login' element={!user? <Login/>:<Navigate to="/"/>}/>
         <Route path='/signup' element={!user?<SignupPage/>:<Navigate to='/'/>}/>
         <Route path='/' element={user?<Home/>:<Navigate to="/login"/>}/>
-        <Route path='/vjti' element={user?<VjtiCustom/>:<Navigate to="/login"/>}/>
+        <Route path='/vjti' element={user?<Vjticustom/>:<Navigate to="/login"/>}/>
         <Route path='/generate_snap' element={user?<SnapPage/>:<Navigate to="/login"/>}/>
         <Route path='/discuss' element={user?<Discuss/>:<Navigate to="/login"/>}/>
         <Route path='/experience/:id' element={user?<Experience/>:<Navigate to="/login"/>}/>
